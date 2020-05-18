e0c1cc1fc70c4bbcdb1659a1fa15e08f7f85c3de
phamus <sphamus@gmail.com>
2020-05-18 02:09:49 +0100

    Socket.io for create post

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,10 +1,11 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const { validationResult } = require('express-validator/check');
+const { validationResult } = require("express-validator/check");
 
-const Post = require('../models/post');
-const User = require('../models/user');
+const io = require("../socket");
+const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.getPosts = async (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -12,13 +13,14 @@ exports.getPosts = async (req, res, next) => {
   try {
     const totalItems = await Post.find().countDocuments();
     const posts = await Post.find()
+      .populate("creator")
       .skip((currentPage - 1) * perPage)
       .limit(perPage);
 
     res.status(200).json({
-      message: 'Fetched posts successfully.',
+      message: "Fetched posts successfully.",
       posts: posts,
-      totalItems: totalItems
+      totalItems: totalItems,
     });
   } catch (err) {
     if (!err.statusCode) {
@@ -29,14 +31,15 @@ exports.getPosts = async (req, res, next) => {
 };
 
 exports.createPost = async (req, res, next) => {
+  console.log("I got here");
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const error = new Error('Validation failed, entered data is incorrect.');
+    const error = new Error("Validation failed, entered data is incorrect.");
     error.statusCode = 422;
     throw error;
   }
   if (!req.file) {
-    const error = new Error('No image provided.');
+    const error = new Error("No image provided.");
     error.statusCode = 422;
     throw error;
   }
@@ -47,17 +50,24 @@ exports.createPost = async (req, res, next) => {
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: req.userId
+    creator: req.userId,
   });
+
   try {
     await post.save();
     const user = await User.findById(req.userId);
     user.posts.push(post);
     await user.save();
+
+    io.getIO().emit("posts", {
+      action: "create",
+      post: { ...post._doc, creator: { _id: req.userId, name: user.name } },
+    });
+
     res.status(201).json({
-      message: 'Post created successfully!',
+      message: "Post created successfully!",
       post: post,
-      creator: { _id: user._id, name: user.name }
+      creator: { _id: user._id, name: user.name },
     });
   } catch (err) {
     if (!err.statusCode) {
@@ -72,11 +82,11 @@ exports.getPost = async (req, res, next) => {
   const post = await Post.findById(postId);
   try {
     if (!post) {
-      const error = new Error('Could not find post.');
+      const error = new Error("Could not find post.");
       error.statusCode = 404;
       throw error;
     }
-    res.status(200).json({ message: 'Post fetched.', post: post });
+    res.status(200).json({ message: "Post fetched.", post: post });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -89,7 +99,7 @@ exports.updatePost = async (req, res, next) => {
   const postId = req.params.postId;
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    const error = new Error('Validation failed, entered data is incorrect.');
+    const error = new Error("Validation failed, entered data is incorrect.");
     error.statusCode = 422;
     throw error;
   }
@@ -100,19 +110,19 @@ exports.updatePost = async (req, res, next) => {
     imageUrl = req.file.path;
   }
   if (!imageUrl) {
-    const error = new Error('No file picked.');
+    const error = new Error("No file picked.");
     error.statusCode = 422;
     throw error;
   }
   try {
-    const post = await Post.findById(postId);
+    const post = await (await Post.findById(postId)).populated("creator");
     if (!post) {
-      const error = new Error('Could not find post.');
+      const error = new Error("Could not find post.");
       error.statusCode = 404;
       throw error;
     }
     if (post.creator.toString() !== req.userId) {
-      const error = new Error('Not authorized!');
+      const error = new Error("Not authorized!");
       error.statusCode = 403;
       throw error;
     }
@@ -123,7 +133,8 @@ exports.updatePost = async (req, res, next) => {
     post.imageUrl = imageUrl;
     post.content = content;
     const result = await post.save();
-    res.status(200).json({ message: 'Post updated!', post: result });
+    io.getIO().emit("posts", { action: "update", post: result });
+    res.status(200).json({ message: "Post updated!", post: result });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -138,12 +149,12 @@ exports.deletePost = async (req, res, next) => {
     const post = await Post.findById(postId);
 
     if (!post) {
-      const error = new Error('Could not find post.');
+      const error = new Error("Could not find post.");
       error.statusCode = 404;
       throw error;
     }
     if (post.creator.toString() !== req.userId) {
-      const error = new Error('Not authorized!');
+      const error = new Error("Not authorized!");
       error.statusCode = 403;
       throw error;
     }
@@ -155,7 +166,7 @@ exports.deletePost = async (req, res, next) => {
     user.posts.pull(postId);
     await user.save();
 
-    res.status(200).json({ message: 'Deleted post.' });
+    res.status(200).json({ message: "Deleted post." });
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -164,7 +175,7 @@ exports.deletePost = async (req, res, next) => {
   }
 };
 
-const clearImage = filePath => {
-  filePath = path.join(__dirname, '..', filePath);
-  fs.unlink(filePath, err => console.log(err));
+const clearImage = (filePath) => {
+  filePath = path.join(__dirname, "..", filePath);
+  fs.unlink(filePath, (err) => console.log(err));
 };
--- routes/feed.js
+++ routes/feed.js
@@ -1,45 +1,74 @@
-const express = require('express');
-const { body } = require('express-validator/check');
+const express = require("express");
+const { body } = require("express-validator/check");
 
-const feedController = require('../controllers/feed');
-const isAuth = require('../middleware/is-auth');
+const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
+const multer = require("multer");
+
+const path = require("path");
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: fileStorage,
+  limits: {
+    fileSize: 5242880,
+  },
+  fileFilter,
+});
 
 // GET /feed/posts
-router.get('/posts', isAuth, feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 // POST /feed/post
 router.post(
-  '/post',
+  "/post",
   isAuth,
+  upload.single("image"),
   [
-    body('title')
-      .trim()
-      .isLength({ min: 5 }),
-    body('content')
-      .trim()
-      .isLength({ min: 5 })
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
   ],
   feedController.createPost
 );
 
-router.get('/post/:postId', isAuth, feedController.getPost);
+router.get("/post/:postId", isAuth, feedController.getPost);
 
 router.put(
-  '/post/:postId',
+  "/post/:postId",
   isAuth,
+
   [
-    body('title')
-      .trim()
-      .isLength({ min: 5 }),
-    body('content')
-      .trim()
-      .isLength({ min: 5 })
+    body("title").trim().isLength({ min: 5 }),
+    body("content").trim().isLength({ min: 5 }),
   ],
   feedController.updatePost
 );
 
-router.delete('/post/:postId', isAuth, feedController.deletePost);
+router.delete("/post/:postId", isAuth, feedController.deletePost);
 
 module.exports = router;
